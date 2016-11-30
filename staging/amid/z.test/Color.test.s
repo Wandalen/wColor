fdbc8d8dd95f9de747f1233fe0c408219bc50486
volodymyrmigdal <volodymyr.migdal@gmail.com>
2016-11-30 17:59:17 +0200

    Add colorNearest tests

--- staging/amid/z.test/Color.test.s
+++ staging/amid/z.test/Color.test.s
@@ -49,6 +49,83 @@ var simplest = function( test )
 
 //
 
+var colorNearest = function( test )
+{
+  test.description = 'white1';
+  var color = [ 1, 1, 1 ];
+  var got = _.colorNearest( color );
+  var expected = 'white';
+  test.identical( got,expected );
+
+  test.description = 'white2';
+  var color = 'ffffff';
+  var got = _.colorNearest( color );
+  var expected = 'white';
+  test.identical( got,expected );
+
+  test.description = 'rgba a1 ';
+  var color =  [ 1, 1, 1, 0.3 ];
+  var got = _.colorNearest( color );
+  var expected = 'transparent';
+  test.identical( got,expected );
+
+  test.description = 'rgba a2';
+  var color =  [ 1, 1, 1, 0.9 ];
+  var got = _.colorNearest( color );
+  var expected = 'white';
+  test.identical( got,expected );
+
+  test.description = 'rgb a1';
+  var color =  [ 0.5, 0, 0.1 ];
+  var got = _.colorNearest( color );
+  var expected = 'maroon';
+  test.identical( got,expected );
+
+  test.description = 'rgb a2';
+  var color =  [ 255, 0, 1 ];
+  var got = _.colorNearest( color );
+  var expected = 'blue';
+  test.identical( got,expected );
+
+  test.description = 'rgb a3';
+  var color =  [ 0.2, 0, 0.3 ];
+  var got = _.colorNearest( color );
+  var expected = 'olive green';
+  test.identical( got,expected );
+
+  test.description = 'rgb a4';
+  var color =  [ 0.3, 0, 0.3 ];
+  var got = _.colorNearest( color );
+  var expected = 'purple';
+  test.identical( got,expected );
+
+  test.description = 'hex a1';
+  var color =  'aaaaaa';
+  var got = _.colorNearest( color );
+  var expected = 'silver';
+  test.identical( got,expected );
+
+  test.description = 'empty string';
+  var color =  '';
+  var got = _.colorNearest( color );
+  var expected =  false;
+  test.identical( got,expected );
+
+  test.description = 'empty array';
+  var color =  [];
+  var got = _.colorNearest( color );
+  var expected =  false;
+  test.identical( got,expected );
+
+  test.description = 'no args';
+  test.shouldThrowError( function()
+  {
+    _.colorNearest(  );
+  })
+}
+
+//
+
 var Proto =
 {
 
@@ -58,6 +135,7 @@ var Proto =
   {
 
     simplest : simplest,
+    colorNearest : colorNearest
 
   },
 
