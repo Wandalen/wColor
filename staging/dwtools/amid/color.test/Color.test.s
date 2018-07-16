f929303feaf76365921f38df95ec27b482e733ea
Wandalen <wandalen.me@gmail.com>
2018-07-16 18:05:48 +0300

    sync

--- staging/dwtools/amid/color.test/Color.test.s
+++ staging/dwtools/amid/color.test/Color.test.s
@@ -1,6 +1,6 @@
 ( function _Color_test_s_( ) {
 
-'use strict';
+'use strict'; /**/
 
 if( typeof module !== 'undefined' )
 {
--- staging/dwtools/amid/color/Color.s
+++ staging/dwtools/amid/color/Color.s
@@ -1,6 +1,6 @@
 (function _Color_s_() {
 
-'use strict';
+'use strict'; /**/
 
 if( typeof module !== 'undefined' )
 {
