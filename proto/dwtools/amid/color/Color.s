f81826b0ce50c91d576a4dceaedc87cded5b630d
Wandalen <wandalen.me@gmail.com>
2018-11-05 17:08:59 +0200

    auto

--- proto/dwtools/amid/color/Color.s
+++ proto/dwtools/amid/color/Color.s
@@ -1400,7 +1400,7 @@ _.mapSupplement( _.color.ColorMap,ColorMapShell );
 
 if( typeof module !== 'undefined' )
 if( _global_.WTOOLS_PRIVATE )
-delete require.cache[ module.id ];
+{ /* delete require.cache[ module.id ]; */ }
 
 if( typeof module !== 'undefined' && module !== null )
 module[ 'exports' ] = Self;
