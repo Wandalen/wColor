f03b7643f81a376a1aacd2db6bc37a5cc6776491
Wandalen <wandalen.me@gmail.com>
2020-02-07 15:34:53 +0200

    version 0.3.163

--- proto/dwtools/amid/color.test/Color.test.s
+++ proto/dwtools/amid/color.test/Color.test.s
@@ -5,22 +5,23 @@
 if( typeof module !== 'undefined' )
 {
 
-  if( typeof _global_ === 'undefined' || !_global_.wBase )
-  {
-    let toolsPath = '../../../dwtools/Base.s';
-    let toolsExternal = 0;
-    try
-    {
-      require.resolve( toolsPath );
-    }
-    catch( err )
-    {
-      toolsExternal = 1;
-      require( 'wTools' );
-    }
-    if( !toolsExternal )
-    require( toolsPath );
-  }
+  require( '../../Tools.s' );
+  // if( typeof _global_ === 'undefined' || !_global_.wBase )
+  // {
+  //   let toolsPath = '../../../dwtools/Base.s';
+  //   let toolsExternal = 0;
+  //   try
+  //   {
+  //     require.resolve( toolsPath );
+  //   }
+  //   catch( err )
+  //   {
+  //     toolsExternal = 1;
+  //     require( 'wTools' );
+  //   }
+  //   if( !toolsExternal )
+  //   require( toolsPath );
+  // }
 
   require( '../color/Color.s' );
 
--- proto/dwtools/amid/color/Color.s
+++ proto/dwtools/amid/color/Color.s
@@ -20,22 +20,23 @@
 if( typeof module !== 'undefined' )
 {
 
-  if( typeof _global_ === 'undefined' || !_global_.wBase )
-  {
-    let toolsPath = '../../../dwtools/Base.s';
-    let toolsExternal = 0;
-    try
-    {
-      toolsPath = require.resolve( toolsPath );
-    }
-    catch( err )
-    {
-      toolsExternal = 1;
-      require( 'wTools' );
-    }
-    if( !toolsExternal )
-    require( toolsPath );
-  }
+  require( '../../Tools.s' );
+  // if( typeof _global_ === 'undefined' || !_global_.wBase )
+  // {
+  //   let toolsPath = '../../../dwtools/Base.s';
+  //   let toolsExternal = 0;
+  //   try
+  //   {
+  //     toolsPath = require.resolve( toolsPath );
+  //   }
+  //   catch( err )
+  //   {
+  //     toolsExternal = 1;
+  //     require( 'wTools' );
+  //   }
+  //   if( !toolsExternal )
+  //   require( toolsPath );
+  // }
 
 }
 
