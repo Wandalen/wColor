5cb463b61049154f9254c3af83240a1972b8fa4d
Jack Shell <jackshel45@gmail.com>
2021-02-03 10:32:40 +0200

    Renamed files Color*.s -> *.s

--- proto/wtools/amid/color/include/Mid.s
+++ proto/wtools/amid/color/include/Mid.s
@@ -9,8 +9,8 @@ if( typeof module !== 'undefined' )
 {
   let _ = require( './Basic.s' );
   require( '../l3/Color.s' );
-  require( '../l3/ColorCmyk.s' );
-  require( '../l3/ColorCmyka.s' );
+  require( '../l3/Cmyk.s' );
+  require( '../l3/Cmyka.s' );
   module[ 'exports' ] = _global_.wTools;
 }
 
