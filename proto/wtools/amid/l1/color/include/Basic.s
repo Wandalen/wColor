b389c7a939d48117207b87d5de8f7a16c70fd5e9
wandalen <wandalen.me@gmail.com>
2021-04-06 16:07:33 +0300

    .

--- proto/wtools/amid/l1/color/include/Basic.s
+++ proto/wtools/amid/l1/color/include/Basic.s
@@ -7,7 +7,7 @@
 
 if( typeof module !== 'undefined' )
 {
-  let _ = require( '../../../../../wtools/Tools.s' );
+  let _ = require( '../../../../../node_modules/Tools' );
   module[ 'exports' ] = _global_.wTools;
 }
 
